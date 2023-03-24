0a2f06925bbfc093790c71bca232f1cdfc80f5f5
hp <hp@DESKTOP-H56K038>
2023-03-24 09:48:04 +0500

    Добавьте файлы проекта.

---
+++ Tub son/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Tub_son
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number, countt=0;
+            Console.Write("Enter the number: ");
+            number=int.Parse(Console.ReadLine());
+            for (int i = 1; i <=number;i ++)
+            {
+                  if(number%i==0)
+                  {
+                    countt++;
+                  }
+
+
+            }
+            if(countt==2) 
+            {
+
+                Console.WriteLine("Tub son!");
+
+            }
+            else
+            {
+                Console.WriteLine("Tub emas!");
+            }
+        }
+    }
+}
